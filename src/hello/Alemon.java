59dbb18b165878923100756c7b88aadf5a20e213
anderson-simelli <asimelli@minha.fag.edu.br>
2026-03-06 19:47:29 -0300

    Adicionando classe Alemon

---
+++ src/hello/Alemon.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Alemon {
+
+    public static void falar() {
+        System.out.println("Hallo Welt!");
+    }
+
+}
--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,10 +2,11 @@ package hello;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Hello World");
-		
-	}
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        Alemon.falar();
+
+    }
 
 }
