738b7d242751a66f67d1814505950a4abe360123
anderson-simelli <asimelli@minha.fag.edu.br>
2026-03-08 17:14:44 -0300

    Atividade corrigida

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 
         System.out.println("Hello World");
         Alemon.falar();
-
+        Italiaano.falar();
     }
 
 }
---
+++ Italiaano.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Italiaano {
+
+    public static void falar() {
+        System.out.println("Ciao Mondo!");
+    }
+
+}
